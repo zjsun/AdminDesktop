7a320781955e12fa2d129f74d4ddc4c9314d64dd
Alex.Sun <zjsun@126.com>
2017-09-14 13:21:00 +0800

    misc fix

--- src/renderer/libs/MainFooter.vue
+++ src/renderer/libs/MainFooter.vue
@@ -3,7 +3,7 @@
     <footer class="main-footer">
         <!-- To the right -->
         <div class="pull-right hidden-xs">
-            版本：3.1.6@20170911
+            版本：0.0.1@20170911
         </div>
         <!-- Default to the left -->
         <strong>&copy; 2016~2017 <a>Xxx, Inc.</a></strong>
